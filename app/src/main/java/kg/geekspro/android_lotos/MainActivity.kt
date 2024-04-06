09a74624e870b5a9b148249b665afe5c1f6e08bf
Kadyrbek <kadyrtashtemir@gmail.com>
2024-04-05 22:15:25 -0700

    test second

--- app/src/main/java/kg/geekspro/android_lotos/MainActivity.kt
+++ app/src/main/java/kg/geekspro/android_lotos/MainActivity.kt
@@ -15,15 +15,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            //dkjsflkjsdlkjlfkjs
-            // fix my bag
-            // my first pr
-            // test new arch
-            // ololo lolo
+            // pls test
         }
     }
-    ////////
-    ///////ifhjiowhiowhoiwehfiowehfoiwehfioewhfoiwehfjfiowepiofwefphwehfpweihfpoweihfpiwehfpiewhpifewhpifhwepfhwephfpwehfpiwehfpweihpiwehp
-    ///hhhhhhhhhhhh
 }
-//
