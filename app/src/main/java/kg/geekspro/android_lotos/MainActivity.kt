fc97f1f377a24a4b01e8efac0f28cd9a60d970c7
Baychik <ergeshovbaistan@gmail.com>
2024-04-09 23:42:07 +0600

    the new commit

--- app/src/main/java/kg/geekspro/android_lotos/MainActivity.kt
+++ app/src/main/java/kg/geekspro/android_lotos/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+
+        println("Это MainActivity")
+        // добавил println()
     }
 }
