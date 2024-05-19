0be508495a088283bd81158d6026c8cbe990d046
Tashpolotov <Aza662485@gmail.com>
2024-05-19 18:37:49 +0600

    added FCM token

--- app/src/main/java/kg/geekspro/android_lotos/ui/fragments/pesonaldata/RefactorDataFragment.kt
+++ app/src/main/java/kg/geekspro/android_lotos/ui/fragments/pesonaldata/RefactorDataFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import kg.geekspro.android_lotos.R
 import kg.geekspro.android_lotos.databinding.FragmentRefactorDataBinding
@@ -77,6 +78,7 @@ class RefactorDataFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.apply {
             viewModel.getProfileData(pref.getAccessToken()!!).observe(viewLifecycleOwner) {
                 if (it != null) {
@@ -95,7 +97,6 @@ class RefactorDataFragment : Fragment() {
                             dateOfBirth = binding.etDateOfBirth.text.toString(),
                             address = binding.etAddress.text.toString()
                         )
-
                         refactorViewModel.putData(refactorData).observe(viewLifecycleOwner) {
                             findNavController().navigate(R.id.profileFragment)
                         }
