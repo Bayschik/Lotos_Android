a1093851c033139affe8ae2c2517ff5e37f2338f
Baychik <ergeshovbaistan@gmail.com>
2024-09-03 22:54:11 +0600

    я здаюсь

--- app/src/main/java/kg/geekspro/android_lotos/activity/MyFirebaseMessagingService.kt
+++ app/src/main/java/kg/geekspro/android_lotos/activity/MyFirebaseMessagingService.kt
@@ -1,42 +1,62 @@
 package kg.geekspro.android_lotos.activity
 
-import android.app.NotificationManager
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kg.geekspro.android_lotos.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
+    @SuppressLint("MissingPermission")
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        remoteMessage.data.let {
-            val targetFragment = "orderHistoryFragment"
-            val id = it["channelId"]
+        remoteMessage.let {
+            val id = it.data["channelId"]
 
             val intent = Intent(this, OrdersActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                putExtra("targetFragment", targetFragment)
             }
+            /*val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+                // Add the intent, which inflates the back stack.
+                addNextIntentWithParentStack(intent)
+                // Get the PendingIntent containing the entire back stack.
+                getPendingIntent(0,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            }*/
 
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
             val notificationBuilder = NotificationCompat.Builder(this, id!!)
-                .setChannelId("notify")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setSmallIcon(R.drawable.ic_notifications)
-                .setAutoCancel(true)
+                .setContentTitle(it.notification?.title)
+                .setContentText(it.notification?.body)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
 
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(0, notificationBuilder.build())
+            with(NotificationManagerCompat.from(this)) {
+                if (ActivityCompat.checkSelfPermission(
+                        this@MyFirebaseMessagingService,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return@with
+                }
+                // notificationId is a unique int for each notification that you must define.
+                notify(0, notificationBuilder.build())
+            }
         }
-        Log.d("Notification", "${ remoteMessage.messageId}")
-        Log.d("Notification", "${ remoteMessage.data}")
-        Log.d("Notification", "${ remoteMessage.notification?.channelId}")
-        Log.d("Notification", "${ remoteMessage.notification?.body}")
+
+        Log.d("Notification", "${remoteMessage.messageId}")
+        Log.d("Notification", "${remoteMessage.data}")
+        Log.d("Notification", "${remoteMessage.notification?.channelId}")
+        Log.d("Notification", "${remoteMessage.notification?.body}")
     }
 }
--- app/src/main/java/kg/geekspro/android_lotos/activity/OrdersActivity.kt
+++ app/src/main/java/kg/geekspro/android_lotos/activity/OrdersActivity.kt
@@ -1,16 +1,16 @@
 package kg.geekspro.android_lotos.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kg.geekspro.android_lotos.OrderHistoryFragment
 import kg.geekspro.android_lotos.R
 import kg.geekspro.android_lotos.databinding.ActivityOrdersBinding
 
 @AndroidEntryPoint
 class OrdersActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityOrdersBinding
+    private lateinit var binding: ActivityOrdersBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOrdersBinding.inflate(layoutInflater)
@@ -22,5 +22,10 @@ class OrdersActivity : AppCompatActivity() {
                 navController.navigate(R.id.orderHistoryFragment)
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        findNavController(R.id.nav_host_fragment).navigate(R.id.orderHistoryFragment)
     }
 }
